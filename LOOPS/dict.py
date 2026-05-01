0422d7f55f52fb69002891ff79738e7c944dc3b7
ms32108 <manisai120803@gmail.com>
2026-05-01 16:12:46 +0530

    dict 2

--- LOOPS/dict.py
+++ LOOPS/dict.py
@@ -3,4 +3,15 @@ students = {
     "Sai":"Ch",
     "Raju":"C"
 }
+
+"""
 print(students["Mani"])
+print(students["Sai"])
+print(students["Raju"])
+"""
+
+for student in students:
+    print(student) # print only keys
+    print(student,students[student]) # print keys with values too
+    
+    
