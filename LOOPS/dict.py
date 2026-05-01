cc0322d28c652f849a92253e415378d5a92c6eed
ms32108 <manisai120803@gmail.com>
2026-05-01 16:14:33 +0530

    dict 3

--- LOOPS/dict.py
+++ LOOPS/dict.py
@@ -13,5 +13,6 @@ print(students["Raju"])
 for student in students:
     print(student) # print only keys
     print(student,students[student]) # print keys with values too
+    print(student,students[student],sep=",") # print keys with values too with seperator
     
     
