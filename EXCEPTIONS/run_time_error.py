abf1191f4ee25ae88cfc3ef94addef033bb5c197
ms32108 <manisai120803@gmail.com>
2026-05-12 13:51:45 +0530

    c6

---
+++ EXCEPTIONS/run_time_error.py
@@ -0,0 +1,12 @@
+while 1:
+    x =int(input("What is x??  "))
+    print (f"x is {x}") 
+
+""" if enter "cat" which can't convert into int as its not number 
+    which is an exception  , its a Value type error
+
+
+    so we write with error handling code 
+"""
+
+## we use "try" key word to handle this
---
+++ EXCEPTIONS/try_except.py
@@ -0,0 +1,5 @@
+try:
+    x =int(input("What is x??  "))
+    print (f"x is {x}") 
+except ValueError:
+    print("x is not an interger")
