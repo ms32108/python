d8a69756dc50341583e7c119e9956337cd56be27
ms32108 <manisai120803@gmail.com>
2026-05-16 15:09:05 +0530

    sys Index error

--- Libraries/Statistics/average.py
+++ Libraries/Statistics/average.py
@@ -1,5 +1,5 @@
 import statistics
 
-class_scores=[23,323,23,32,32,32,32,342,324,4,343,43,43,5353,535,353]
+class_scores=[23,32,23,42,34,40,43,43,43,53,55,50]
 avg=statistics.mean(class_scores)
-print(avg)
+print(int(avg))
---
+++ Libraries/sys/name.py
@@ -0,0 +1,7 @@
+import sys
+
+print("hello, my name is", sys.argv[1])
+# python name.py manisai (run like this )
+# o/p---> hello, my name is manisai
+print("hello, my name is", sys.argv[0])
+# o/p---> hello, my name is name.py
