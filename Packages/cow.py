f72b21d91db3af61daebdacfb17c4416c64414eb
ms32108 <manisai120803@gmail.com>
2026-05-16 23:58:31 +0530

    Packages cowsay

---
+++ Packages/cow.py
@@ -0,0 +1,21 @@
+import cowsay
+import sys
+
+if len(sys.argv) ==2:
+    cowsay.cow("helo, "+sys.argv[1])
+
+"""
+ O/P---   
+      __________
+| helo, Mani |
+  ==========
+          \
+           \
+             ^__^
+             (oo)\_______
+             (__)\       )\/\
+                 ||----w |
+                 ||     ||
+    
+    
+    """
